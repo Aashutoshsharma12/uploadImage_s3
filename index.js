aa7fcd87508d3c301122d47462f6b778665f0368
Aashutoshsharma12 <aashutoshpandit1212@gmail.com>
2023-05-31 16:52:20 +0530

    commit

---
+++ index.js
@@ -0,0 +1,113 @@
+const express = require('express')
+const Router = require('express')
+const fileUpload = require('express-fileupload')
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload({
+    limits: {
+        fileSize: 50 * 1024 * 1024
+    }
+}));
+const AWS = require('aws-sdk');
+// import AWS from 'aws-sdk'
+const BucketName = "graph-imageupload"
+const region = 'ap-south-1'
+// configure it with your AWS credentials:
+AWS.config.update({ region: 'ap-south-1' });
+var s3 = new AWS.S3({
+    credentials: {
+        accessKeyId: 'AKIARXOWZL5DMJY4C6HJ',
+        secretAccessKey: 'M9pLQFkoZkPckcPkyqGfs1bt/2Etkfvi/t5hOt2s'
+    }
+});
+
+
+//Upload Image 
+app.post('/uploadImage', (req, res) => {
+    try {
+        const uploadParams = {
+            Bucket: BucketName,
+            Key: req.files.files.name,
+            Body: Buffer.from(req.files.files.data), // Image buffer or stream
+            ACL: req.body.ACL ? req.body.ACL : "private" // Set ACL to public-read for public access //private
+        };
+        s3.upload(uploadParams, async (err, data) => {
+            if (err) {
+                return res.status(400).json({ message: err });
+            } else {
+                return res.status(200).json({ message: "Image uploaded successfully", url: data.Location })
+            }
+        });
+    } catch (err) {
+        res.json({ message: err })
+    }
+}
+)
+
+// Delete Image
+app.post('/deleteImage', (req, res) => {
+    try {
+        const imageUrl = req.body.imageUrl
+        const lastSlashIndex = imageUrl.lastIndexOf('/');
+        const filename = imageUrl.substring(lastSlashIndex + 1);
+
+        const deleteParams = {
+            Bucket: BucketName, //Bucket Name
+            Key: filename, //Image Name 
+        }
+        s3.deleteObject(deleteParams, function (err, data) {
+            if (err) {
+                res.json({ errr: err })
+            } else {
+                res.json({ data, message: "Image delete successfully" })
+            }
+        });
+    } catch (err) {
+        res.json({ message: err })
+    }
+}
+)
+
+//image list ***************************************//
+app.get('/imageList', (req, res) => {
+    const listParams = {
+        Bucket: BucketName
+    };
+    s3.listObjectsV2(listParams, function (err, data) {
+        if (err) {
+            return res.json({ message: err.message })
+        } else {
+            const imageDetails = data.Contents
+            var array = []
+            imageDetails.map((data) => {
+                array.push({ "imageUrl": `https://${BucketName}.s3.${region}.amazonaws.com/${data.Key}` })
+            })
+            return res.json({ data: array })
+        }
+    });
+}
+)
+
+//Private image url convert into public url for some time//
+app.post('/pre_signedUrl', (req, res) => {
+    try {
+        const imageUrl = req.body.imageUrl
+        const lastSlashIndex = imageUrl.lastIndexOf('/');
+        const filename = imageUrl.substring(lastSlashIndex + 1);
+
+        const getURLParams = {
+            Bucket: BucketName, //Bucket Name
+            Key: filename, //Image Name
+            Expires: req.body.expiredTime // Expiration time in seconds (e.g., 1 hour)
+        };
+        const imageURL = s3.getSignedUrl('getObject', getURLParams);
+        return res.json({ pre_signed_url: imageURL })
+    } catch (err) {
+        res.json({ message: err })
+    }
+}
+)
+app.listen(5009, () => {
+    console.log('Server is running on 5009 port')
+})
